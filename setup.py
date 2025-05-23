34e26fe81e47781eb0fcbbae7891fe61d0a9e496
kdb04 <nikhil.srivatsa1@gmail.com>
2025-05-23 15:08:44 +0530

    [setup] setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="stocktracker",
+    version="0.0.1",
+    description="Stock Tracker",
+    author="Nikhil",
+    author_email="admin@example.com",
+    packages=find_packages(),
+    zip_safe=False,
+    include_package_data=True
+)
